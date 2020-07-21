811931339dc5b53c975f9b35024f4dc0e724eb14
Ruslan <mac@MacBook-Pro-Ruslan.local>
2020-07-21 15:11:21 +0300

    test

--- course/models.py
+++ course/models.py
@@ -2,7 +2,21 @@ from django.db import models
 from django.utils.translation import ugettext as _
 from django.utils.safestring import mark_safe
 
+from django.urls import reverse
+from os import listdir
+from os.path import isfile, join, isdir
+import markdown
+from shop.settings import DATA_DIR, VIDEO_DIR
+import os
+
 # Create your models here.
+def parse_md(txt):
+    try:
+        txt = txt.decode('UTF-8')
+    except:
+        pass
+    return mark_safe(markdown.markdown(txt,extensions=['extra', 'smarty'], output_format='html5'))
+
 class Course(models.Model):
     name = models.CharField(max_length=250, verbose_name=_(u'Name'), blank=True, null = True)
     desc = models.TextField(verbose_name=_(u'Description'), blank=True, null = True)
@@ -15,7 +29,26 @@ class Course(models.Model):
 
 
 
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('course_detail', kwargs={'slug': self.name_slug })
+
 
     @property
     def image_tag(self):
         return mark_safe('<img width="60" src="%s" />' % self.image.url)
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=250, blank=True, verbose_name=_(u'Name'))
+    number = models.IntegerField(default=0, verbose_name=_(u'Number'))
+    is_active = models.BooleanField(verbose_name=_('Is main?'), default=False)
+    image = models.ImageField(blank=True, verbose_name=_(u'Image'), upload_to='lessons_images', null=True)
+    course = models.ForeignKey(Course, verbose_name=_(u'Course'), on_delete=models.CASCADE)
+    name_slug = models.CharField(verbose_name='Name slug', max_length=250, blank=True)
+
+    def get_absolute_url(self):
+        return reverse('lesson_detail', kwargs={'slug': self.name_slug })
+
+    
--- course/views.py
+++ course/views.py
@@ -3,3 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 def index(r):
     return render(r, 'course.html', {'var': 'value'})
+    #rep
